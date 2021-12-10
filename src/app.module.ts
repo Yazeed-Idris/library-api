0bf139255f6bf9a3620d4357e7eec76cfed8737c
Yazeed Idris <s201730270@kfupm.edu.sa>
2021-12-10 18:03:32 +0300

    - Added the configuration and orm modules to connect to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LibrarianModule } from './librarian/librarian.module';
 import { MemberModule } from './member/member.module';
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [LibrarianModule, MemberModule],
+  imports: [
+    LibrarianModule,
+    MemberModule,
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      port: 5432,
+      autoLoadEntities: true,
+      entities: [
+
+      ],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
